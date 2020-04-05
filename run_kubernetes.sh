f91e5f4436d853ebe1675fc0ebac2a81d39409af
agrinshpon <agrinshpon@gmail.com>
2020-04-05 18:27:14 -0400

    Copying files from Udacity repo

---
+++ run_kubernetes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="agrinshpon/app"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run mlapp\
+	--generator=run-pod/vi\
+	--image=$dockerpath\
+	--port=80 --labels app=mlapp
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward mlapp 8000:80
